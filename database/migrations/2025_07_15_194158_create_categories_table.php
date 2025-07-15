bb3335db12d98f21caf4c598b7bcfda7fadfe15b
mervanoktan <230541144@firat.edu.tr>
2025-07-15 23:43:39 +0300

    tasks tablosuna foreing key eklendi.

---
+++ database/migrations/2025_07_15_194158_create_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string(column: 'name');
+            $table->boolean(column: 'is_active')->default(value: 1)->comment(comment: '0-inaktif 1-aktif');
+            $table->softdeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
